eaaf565b1114e1befc6d4409044fd7d77d36d5e3
Gowtham Kumar <72903548+KGK06@users.noreply.github.com>
2021-06-23 19:12:36 +0530

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -8,7 +8,7 @@ AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" "))
 
 
 #################################### FOR PRIVATE ################################################
-@Client.on_message((filters.document|filters.video|filters.audio|filters.photo) & filters.incoming & filters.AUTH_USERS & ~filters.edited & ~filters.channel)
+@Client.on_message((filters.document|filters.video|filters.audio|filters.photo) & filters.incoming & filters.user(AUTH_USERS) & ~filters.edited & ~filters.channel)
 async def storefile(c, m):
     send_message = await m.reply_text("**Processing...**", quote=True)
     if m.document:
